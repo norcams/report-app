a8efde32f9f8a994f9755339bed5ecca6cd59f57
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-08-30 17:01:47 +0200

    Updated app and moved config file

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from oauth.models import db as oauth_db
 app = connexion.App(__name__, specification_dir='./')
 
 # Production server config
-app.app.config.from_pyfile('production.cfg')
+app.app.config.from_pyfile('/etc/himlar/production.cfg')
 
 # This will set the need env TOKENINFO_URL from production.cfg
 token_url = app.app.config.get('TOKENINFO_URL')
@@ -17,6 +17,10 @@ if token_url:
 # Read the api.yaml file to configure the endpoints
 app.add_api('api/api.yaml', strict_validation=True)
 app.add_api('oauth/oauth2.yaml', strict_validation=True)
+
+# Database setup - this will make import db work inside packages without more sql config
+oauth_db.init_app(app.app)
+api_db.init_app(app.app)
 
 # Default landing page
 @app.route("/")
@@ -30,6 +34,4 @@ if __name__ == '__main__':
     host = app.app.config.get('HOST', '0.0.0.0')
     debug = app.app.config.get('DEBUG', False)
     port = app.app.config.get('PORT', 5000)
-    oauth_db.init_app(app.app)
-    api_db.init_app(app.app)
     app.run(host=host, port=port, debug=debug)
