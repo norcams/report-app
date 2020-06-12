81424533a7b004d74841d7324417837e2c9c0c49
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2020-06-12 09:02:58 +0200

    Add python3 mod_wsgi to dependendcy

--- setup.py
+++ setup.py
@@ -10,18 +10,20 @@ config = {
     'url': 'https://github.com/norcams/report-app',
     'download_url': 'https://github.com/norcams/report-app',
     'author_email': 'raymond.Kristiansen@uib.no',
-    'version': '0.1',
+    'version': '1.4',
     'install_requires': [
         'flask==1.1.2',
         'connexion[swagger-ui]==2.7.0',
         'Flask-SQLAlchemy==2.4.3',
         'bcrypt',
         'PyMySQL',
-        'Flask-Cors'
+        'Flask-Cors',
+        'mod-wsgi'
     ],
     'packages': ['api', 'oauth'],
     'scripts': [],
-    'name': 'report-api'
+    'name': 'report-api',
+    'options': { "bdist_wheel": {"universal": "1"}},
 }
 
 setup(**config)
