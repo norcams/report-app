1df29ca93af616ca026bfd40a2265cec6b453252
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2020-06-15 08:43:55 +0200

    Added limit for get status message based on date

--- api/status.py
+++ api/status.py
@@ -1,14 +1,15 @@
 from connexion import NoContent
 from flask import current_app as app
-from flask import request
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 from .models import db
 from .models import Status
 
-def get_status(limit, message_type=None):
-    status = Status.query.order_by(Status.id.desc())
+def get_status(limit, limit_days, message_type=None):
+    start_at = datetime.today() - timedelta(days=limit_days + 1)
+    app.logger.info('Start at set to {}'.format(start_at))
+    status = Status.query.filter(Status.timestamp >= start_at).order_by(Status.id.desc())
     if message_type:
-        status = status.filter(Status.message_type==message_type)
+        status = status.filter(Status.message_type == message_type)
     return [s.dump() for s in status.all()][:limit]
 
 def put_status(status):
@@ -17,7 +18,7 @@ def put_status(status):
     status = Status(message=status['message'], message_type=message_type)
     db.session.add(status)
     db.session.commit()
-    return NoContent, 200
+    return NoContent, 201
 
 def delete_status(status_id):
     status = Status.query.filter_by(id=status_id).first()
@@ -26,5 +27,4 @@ def delete_status(status_id):
         db.session.delete(status)
         db.session.commit()
         return NoContent, 204
-    else:
-        return NoContent, 404
+    return NoContent, 404
