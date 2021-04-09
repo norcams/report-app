e1c5246406fced563b4f689bc6825172c2ac1b24
Raymond Kristiansen <raymond.kristiansen@uib.no>
2021-04-09 12:25:38 +0200

    First rc of 1.6 version

--- api/status.py
+++ api/status.py
@@ -3,6 +3,8 @@ from flask import current_app as app
 from datetime import datetime, timedelta
 from .models import db
 from .models import Status
+from feedgen.feed import FeedGenerator
+import pytz
 
 def get_status(limit, limit_days, message_type=None):
     start_at = datetime.today() - timedelta(days=limit_days + 1)
@@ -28,3 +30,23 @@ def delete_status(status_id):
         db.session.commit()
         return NoContent, 204
     return NoContent, 404
+
+def get_rss(limit, limit_days, message_type=None):
+    start_at = datetime.today() - timedelta(days=limit_days + 1)
+    app.logger.info('get_rss: Start set to {}'.format(start_at))
+    status = Status.query.filter(Status.timestamp >= start_at).order_by(Status.timestamp)
+    if message_type:
+        status = status.filter(Status.message_type == message_type)
+
+    fg = FeedGenerator()
+    fg.title('Some Testfeed')
+    fg.language('en')
+    fg.link(href='https://status.nrec.no', length=140)
+    fg.description('test')
+    for s in status.all()[:limit]:
+        fe = fg.add_entry()
+        fe.id(str(s.id))
+        fe.title(s.message)
+        fe.category(category=dict({'term': s.message_type}))
+        fe.published(pytz.utc.localize(s.timestamp))
+    return fg.rss_str(pretty=True), 200
